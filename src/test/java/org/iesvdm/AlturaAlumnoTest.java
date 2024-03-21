32368ae791c41dcf0d07f9049f5c3d21d00c948f
davidvalenzuelafarias <dvalfar1305@g.educaand.es>
2024-03-21 10:08:32 +0100

    Test añade altura

--- src/test/java/org/iesvdm/AlturaAlumnoTest.java
+++ src/test/java/org/iesvdm/AlturaAlumnoTest.java
@@ -1,5 +1,8 @@
 package org.iesvdm;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Array;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,10 +16,27 @@ public class AlturaAlumnoTest {
 
         String[] ArrayNuevo = AlturaAlumno.añadeNombre(array,nombre);
 
-        assertTrue(ArrayNuevo[ArrayNuevo.length-1]==nombre);
-        assertTrue(ArrayNuevo.length==array.length+1);
+        assertSame(ArrayNuevo[ArrayNuevo.length - 1], nombre);
+        assertEquals(ArrayNuevo.length, array.length + 1);
         assertArrayEquals(array,Arrays.copyOfRange(ArrayNuevo,0,array.length));
 
+    }
+
+    @Test
+    void añadealtura()
+    {
+        double[] array = {1.3,2.5,2.8};
+        double alturadefecto = 1.5;
+
+        double[] arrayActual = AlturaAlumno.añadeAltura(array);
+
+        assertEquals(arrayActual[arrayActual.length - 1], alturadefecto);
+
+        assertEquals(arrayActual.length, array.length + 1);
+        assertEquals(alturadefecto, arrayActual[arrayActual.length-1]);
+
+        double[] actual = Arrays.copyOfRange(arrayActual,0,array.length);
+        assertArrayEquals(array,actual);
     }
     
     @Test
@@ -31,23 +51,6 @@ public class AlturaAlumnoTest {
     }
 
     @Test
-    void añadealtura()
-    {
-        double[] array = {1.3,2.5,2.8};
-        double alturadefecto = 1.5;
-
-        double[] arrayActual = AlturaAlumno.añadeAltura(array);
-
-        assertTrue(arrayActual[arrayActual.length-1]==alturadefecto);
-
-        assertTrue(arrayActual.length == array.length+1);
-        assertEquals(alturadefecto, arrayActual[arrayActual.length-1]);
-
-        double[] actual = Arrays.copyOfRange(arrayActual,0,array.length);
-        assertArrayEquals(array,actual);
-    }
-
-    @Test
     void buscaNombre()
     {
         int indice = -1;
@@ -55,28 +58,44 @@ public class AlturaAlumnoTest {
         String nombre = "Jose";
 
         int resultado = AlturaAlumno.buscaNombre(nombres, nombre);
-        assertTrue(indice==resultado);
+        assertEquals(indice, resultado);
     }
 
     @Test
-    void mostrar(){
-        String[] arrayNombre = new String[1];
-        arrayNombre[0]="David";
-        Double[] arrayAltura = new Double[1];
-        arrayAltura[0]=1.80;
+    void mostrar()
+    {
+        String[] nombres = {"Juan", "María", "Pedro"};
+        double[] alturas = {1.75, 1.60, 1.85};
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        AlturaAlumno.mostrar(nombres, alturas);
+
+        String salida = "Juan\t|   1.75\nMaría\t|   1.6\nPedro\t|   1.85\n";
+        assertEquals(salida, outContent.toString());
     }
 
     @Test
     void calculaMaximo(){
-        double [] array = {1.37, 1.60, 1.45};
-        assertTrue(AlturaAlumno.calculaMaximo(array)[0]==1);
-        assertTrue(AlturaAlumno.calculaMaximo(array)[1]==1.60);
+        double[] alturas = {1.75, 1.60, 1.85};
+
+        double[] resultadoMaximo = AlturaAlumno.calculaMaximo(alturas);
+
+        double[] ResultadoMaximoEsperado = {2, 1.85};
+
+        assertArrayEquals(ResultadoMaximoEsperado, resultadoMaximo);
     }
 
     @Test
     void calculaMedia(){
-        double[] array = {1.0, 2.0, 3.0, 4.0, 5.0};
-        assertTrue(AlturaAlumno.calculaMedia(array)==3.0);
+        double[] alturas = {1.75, 1.60, 1.85};
+
+        double media = AlturaAlumno.calculaMedia(alturas);
+
+        double MediaEsperada = (1.75 + 1.60 + 1.85) / alturas.length;
+
+        assertEquals(MediaEsperada, media);
     }
 }
 
